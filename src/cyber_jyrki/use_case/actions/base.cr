a59d2a6e6ed6649038809b5dafd26d3bafc14c3d
Georg Gadinger <nilsding@nilsding.org>
2022-09-06 22:55:08 +0200

    handle imgur albums from reddit submissions

--- src/cyber_jyrki/use_case/actions/base.cr
+++ src/cyber_jyrki/use_case/actions/base.cr
@@ -30,12 +30,12 @@ module CyberJyrki
         # tries to reply to a message with a given image
         #
         # make sure `caption` is properly escaped
-        private def reply_to_message_with_image(message, image_url : String, caption : String)
+        private def reply_to_message_with_image(message, image_url : String, caption : String) : Tourmaline::Message
           image_url_downcase = image_url.downcase
           is_animation = image_url_downcase.ends_with?(".gif") # likely to be an animated gif
 
           # imgur workarounds
-          if image_url_downcase =~ %r{://i.imgur.com/}
+          if image_url_downcase =~ %r{://i\.imgur\.com/}
             image_url = image_url.gsub(/\.gifv$/i, ".gif") # ensure we use .gif instead of .gifv (some html) extension
 
             # replace ".gif" with ".mp4" as it's more efficient
@@ -46,23 +46,55 @@ module CyberJyrki
           end
 
           if is_animation
-            context.message.reply_with_animation(
+            return message.reply_with_animation(
               image_url,
               caption: caption,
               parse_mode: Tourmaline::ParseMode::MarkdownV2,
             )
-
-            return
           end
 
-          context.message.reply_with_photo(
+          message.reply_with_photo(
             image_url,
             caption: caption,
             parse_mode: Tourmaline::ParseMode::MarkdownV2,
           )
         rescue ex
           ::Log.for(self.class).warn { "got #{ex} (#{ex.class}) while trying to send #{image_url}, sending the image url" }
-          context.message.reply("#{markdown_escape image_url}\n\n" + caption, parse_mode: Tourmaline::ParseMode::MarkdownV2)
+          message.reply("#{markdown_escape image_url}\n\n" + caption, parse_mode: Tourmaline::ParseMode::MarkdownV2)
+        end
+
+        # tries to reply to a message with a given set of images
+        private def reply_to_message_with_album(message, image_urls : Array(String), caption : String)
+          return if image_urls.empty?
+          return reply_to_message_with_image(message, image_urls.first, caption) if image_urls.size == 1
+
+          # this will be set to nil after the first url
+          first_caption : String? = caption
+          last_message = message
+
+          image_urls.each_slice(10) do |urls|
+            if urls.size == 1
+              last_message = reply_to_message_with_image(last_message, urls.first, "")
+              next
+            end
+
+            media_group = urls.map do |url|
+              Tourmaline::InputMediaPhoto.new(
+                media: url,
+                caption: first_caption,
+                parse_mode: Tourmaline::ParseMode::MarkdownV2,
+              ).tap { first_caption = nil }
+            end
+
+            messages = last_message.reply_with_media_group(media_group)
+            last_message = messages.last
+          rescue ex
+            ::Log.for(self.class).warn { "got #{ex} (#{ex.class}) while trying to send one of these urls: #{urls.inspect}, sending the image urls" }
+            urls_markdown = urls.map { |url| "- #{url}" }.join("\n")
+            last_message.reply("#{markdown_escape urls_markdown}\n\n" + caption, parse_mode: Tourmaline::ParseMode::MarkdownV2)
+          end
+
+          last_message
         end
       end
     end
--- src/cyber_jyrki/use_case/actions/hears/reddit.cr
+++ src/cyber_jyrki/use_case/actions/hears/reddit.cr
@@ -1,6 +1,9 @@
 require "./base"
 
 require "../../reddit/post/show"
+require "../../imgur/album/show"
+
+require "uri"
 
 module CyberJyrki
   module UseCase
@@ -22,6 +25,8 @@ module CyberJyrki
 
               if post.image?
                 reply_with_image(post)
+              elsif post.url =~ %r{://(?:www\.)?imgur\.com/a/\S+}
+                reply_with_imgur_album(post)
               else
                 reply_with_text(post)
               end
@@ -31,14 +36,45 @@ module CyberJyrki
           end
 
           private def reply_with_image(post)
-            caption = "*#{markdown_escape post.title}*\n_Posted in r/#{markdown_escape post.subreddit} by u/#{markdown_escape post.author}_"
+            caption = base_caption(post)
 
             reply_to_message_with_image(context.message, image_url: post.url, caption: caption)
           end
 
-          private def reply_with_text(post)
-            content = "*#{markdown_escape post.title}*\n_Posted in r/#{markdown_escape post.subreddit} by u/#{markdown_escape post.author}_\n\n#{markdown_escape post.selftext}"
+          private def reply_with_imgur_album(post)
+            caption = base_caption(post)
+
+            imgur_uri = URI.parse(post.url)
+            path_parts = imgur_uri.path.split("/").reject(&.empty?)
+            return reply_with_link(post) if path_parts.empty?
+
+            album_id = path_parts.last
+            album = UseCase::Imgur::Album::Show.call(album_id)
+            return reply_with_link(post) if album.images.empty?
+
+            image_urls = album.images.reject(&.ad?).map(&.link)
+            reply_to_message_with_album(
+              context.message,
+              image_urls: image_urls,
+              caption: caption
+            )
+          rescue ex
+            Log.error { "got #{ex} (#{ex.class} while trying to reply with imgur album #{post.url}, just posting the link ..." }
+            reply_with_link(post)
+          end
+
+          private def reply_with_link(post)
+            content = "#{base_caption(post)}\n\n#{markdown_escape post.url}"
             context.message.reply(content, parse_mode: Tourmaline::ParseMode::MarkdownV2)
+          end
+
+          private def reply_with_text(post)
+            content = "#{base_caption(post)}\n\n#{markdown_escape post.selftext}"
+            context.message.reply(content, parse_mode: Tourmaline::ParseMode::MarkdownV2)
+          end
+
+          private def base_caption(post)
+            "*#{markdown_escape post.title}*\n_Posted in r/#{markdown_escape post.subreddit} by u/#{markdown_escape post.author}_"
           end
         end
       end
